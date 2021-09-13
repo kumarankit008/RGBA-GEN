408346f41b5444fb82c4cc74e17b792d3322cfeb
Naveen kumar <71214273+kumarankit008@users.noreply.github.com>
2021-09-13 20:26:56 +0530

    corrected copy button

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-
+let inputs=document.querySelectorAll(".input");
+let text=document.querySelector(".output");
 function getValue(){
     let red=document.getElementById("red").value;
     let green =document.getElementById("green").value;
     let blue =document.getElementById("blue").value;
     let alpha =document.getElementById("alpha").value;
-    var colour='rgb('+red+','+green+','+blue+','+ alpha +')';
+    var colour='rgba('+red+','+green+','+blue+','+ alpha +')';
     document.body.style.backgroundColor=colour;
     document.querySelector(".output").innerHTML=colour;
-
-   
 
 }
 
 function copy(){
     let element = document.createElement("textarea");
-    element.value=outputBox.textContent;
+    element.value=text.textContent;
     document.body.appendChild(element);
     element.select();
     document.execCommand("copy");
